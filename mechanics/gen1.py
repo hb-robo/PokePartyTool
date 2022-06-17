71f6bd2db21cdc7b8ad6d69707440b964eff0747
hbrown10 <haydenbrown@comcast.net>
2022-06-16 22:29:25 -0400

    built head-to-head matchup simulator for gen1

--- mechanics/gen1.py
+++ mechanics/gen1.py
@@ -71,6 +71,9 @@ def getDefenseStat(moveName, monName):
 def getMovePower(moveName, moveDex=moveDex):
     return int(moveDex.at[moveName,'power'])
 
+def getMoveAccuracy(moveName,moveDex=moveDex):
+    return 1 if moveName == 'swift' else min(0.996, moveDex.at[moveName,'accuracy']/100)
+
 def calculateDamage(mon, moveName, opp, level=50):
     # Some moves do a damage amount relative to the user's level. These moves
     #    ignore STAB and type bonuses, BUT they do 0 damage to Pokemon who are
@@ -81,7 +84,7 @@ def calculateDamage(mon, moveName, opp, level=50):
         else:
             # Psywave does between 1 and 1.5x the user's level in damage.
             if moveName == 'psywave':
-                return random.randRange(1, math.floor(level*1.5))
+                return random.randrange(1, math.floor(level*1.5))
             # Night Shade and Submission do damage equal to the user's level.
             else:
                 return level
@@ -93,7 +96,7 @@ def calculateDamage(mon, moveName, opp, level=50):
             return max(1, math.floor(opp.hp / 2))
         # Supersonic and Dragon Rage do a fixed damage amount.
         else:
-            return moveDex.at[moveName,'power']   
+            return float(moveDex.at[moveName,'power'])  
 
     # The flying move Mirror Move copies the opponent's last-used attack.
     elif moveDex.at[moveName,'subcat'] == 'reflect':
@@ -127,7 +130,7 @@ def calculateDamage(mon, moveName, opp, level=50):
             mod_dmg = math.floor(math.floor(base_dmg*STAB)*(TB*10/10))
 
             if mod_dmg == 0 or mod_dmg == 1:
-                return mod_dmg
+                return float(mod_dmg)
             else:
                 R = random.randrange(217,256)
                 return math.floor(mod_dmg*(R/255))
@@ -136,43 +139,56 @@ def calculateDamage(mon, moveName, opp, level=50):
 #   that tries to encapsulate the overall value of using the move
 #   versus a given opponent. The move with the highest score will
 #   be selected for use.
-def pickMove(monName, oppName):
+def pickMove(mon, opp, level=50):
     optimalMove = ""
     maxMoveScore = -1
-    for move in learnDex[monName]:        
-        if not np.nan(move):
+    for move in learnDex.columns:        
+        if pd.notna(learnDex.at[mon.name, move]) and move != 'index':
             # We're only considering attacking moves here, so status moves are skipped.
-            if moveDex[move]['category'] == 'status':
+            if moveDex.at[move,'category'] == 'status':
                 continue
             # Many of the most powerful moves in the game cause the user to
             #   kill themselves. Because the user always faints first in the
             #   event of a double knockout, these moves will always be skipped.
-            if moveDex[move]['subcat'] in ('suicide', 'counter', 'seed', 'rage', 'ko', 'random', 'status_dep'):
+            if moveDex.at[move,'subcat'] in ['suicide', 'reflect','counter', 'seed', 'rage', 'ko', 'random', 'status_dep']:
                 continue
 
             # First, the move's damage against the opponent is calculated.
-            #   To account for randomness associated with the calculation, we
-            #   use the average of 100 calculations.
-            score = 0
-            for i in range(10):
-                damage += calculateDamage(monName, str(move), oppName, level=50)
-            damage /= 10
+            if moveDex.at[move, 'subcat'] == 'level_dep':
+                if move == 'psywave':
+                    power = (1.5 * level / 2)
+                else:
+                    power = level
+            elif moveDex.at[move, 'subcat'] == 'fixed':
+                if move == 'super fang':
+                    power = math.floor(opp.hp/2)
+                else: 
+                    power = getMovePower(move)
+            else:
+                power = getMovePower(move)
             # Then, we account for the move's accuracy and the number of times
             #    the move hits. Even "max" accuracy is not 100% unless the move is 
             #    Swift. Otherwise, it's equal to 255/256, or 0.996.
-            accuracy = 0
-            if move.index == 'swift':
-                accuracy = 1
-            else:
-                accuracy = min(0.996, moveDex[move]['accuracy']/100) 
-            adj_damage = damage * accuracy * moveDex[move]['avg_hits']
-            score = adj_damage
+            accuracy = getMoveAccuracy(move)
+            score = power * accuracy * moveDex.at[move,'avg_hits']
+
+            # Accounting for high critical chances:
+            critChance = 1 + calculateCritChance(mon.name, move)
+            score *= critChance
+
+            # Accounting for STAB:
+            STAB = calculateSTAB(mon.name, move)
+            score *= critChance
+
+            # Accounting for Type Bonuses:
+            TB = calculateTypeBonus(move, opp.name)
+            score *= TB
 
             # At this stage we have an emergency exit clause. If the currently selected
             #   move is Quick Attack, and using it would kill the opponent, the Pokemon
             #   will always use it, because it has increased priority even if the opponent's
             #   speed is higher.
-            if (adj_damage >= opponent.currentHP) and moveDex[move]['subcat'] == 'quick':
+            if (score >= opp.hp) and moveDex.at[move,'subcat'] == 'quick':
                 optimalMove = 'quick attack'
                 maxMoveScore = score
                 break
@@ -180,28 +196,28 @@ def pickMove(monName, oppName):
             # We also want to account for how many turns the move takes to charge up.
             #    Moves like Fly and Dig aren't checked here because the charge is done 
             #    in a way that protects the user.
-            if moveDex[move]['subcat'] == 'charge':
-                score /= moveDex[move]['avg_turns']
+            if moveDex.at[move,'subcat'] == 'charge':
+                score /= float(moveDex.at[move,'avg_turns'])
             
             # There is a single move (Hyper Beam) that does not charge up but instead
             #   shoots first with a turn of rest afterwards required. If the move is
             #   expected to kill the next turn, this rest turn is ignored. If the move
             #   will not kill, this rest turn must be accounted for.
-            if (moveDex[move]['subcat'] == 'burst') and (adj_damage < opponent.currentHP):
-                score /= moveDex[move]['avg_turns']
+            if (moveDex.at[move,'subcat'] == 'burst') and (power < opp.hp):
+                score /= float(moveDex.at[move,'avg_turns'])
             
             # Next, we account for status conditions by multiplying the score by a
             #    factor of 1 + its affliction chance.
             #    e.g., a 100-score move with 20% chance to poison now has a score of 120.
-            if (moveDex[move]['opp_status']):
-                status_chance = float(moveDex[move]['opp_status_chance'].strip('%'))/100
+            if pd.notna(moveDex.at[move,'opp_status']):
+                status_chance = float(moveDex.at[move,'opp_status_chance'].strip('%'))/100
                 score += math.floor(score * status_chance)
             
             # Similarly, we account for flinch, but only if the user outspeeds its opponent.
             #    If it doesn't, the opponent will never flinch anyway. If the speed is
             #    equal, moving first is a coin flip, so the flinch chance is halved.
-            if (moveDex[move]['flinch'] and mon.speed >= opp.speed):
-                flinch_chance = float(moveDex[move]['flinch'].strip('%'))/100
+            if pd.notna(moveDex.at[move,'flinch']) and (mon.speed >= opp.speed):
+                flinch_chance = float(moveDex.at[move,'flinch'].strip('%'))/100
                 if mon.speed == opp.speed:
                     flinch_chance /= 2
                 score += math.floor(score * flinch_chance)
@@ -209,31 +225,126 @@ def pickMove(monName, oppName):
             # Finally, we account for ways in which the move can affect the user's HP.
             #   "Absorb" type moves heal the user for a percentage of the damage dealt,
             #   so we add the HP the user would heal to the score.
-            if (moveDex[move]['subcat'] == 'absorb'):
-                heal_amt = math.floor(adj_damage * moveDex[move]['heal'])
-                if (mon.maxHP - mon.currentHP) > heal_amt:
+            if (moveDex.at[move,'subcat'] == 'absorb'):
+                heal_amt = math.floor(power * moveDex.at[move,'heal'])
+                health_diff = pokeDex.at[mon.name,'hp'] - mon.hp
+                if health_diff > heal_amt:
                     score += heal_amt
                 else:
-                    score += (mon.maxHP - mon.currentHP)
+                    score += health_diff
             
             #   Moves with recoil inflict damage on the user as a percentage of the damage
             #   inflicted, so we have to account for those. If the recoil would kill the user,
             #   its score is made to be zero to ensure it doesn't pick it.
-            if (moveDex[move]['recoil']):
-                recoil = math.floor(adj_damage * moveDex[move]['recoil'])
-                if ((mon.currentHP - recoil) <= 0):
+            if pd.notna(moveDex.at[move,'recoil']):
+                recoil = math.floor(power * moveDex.at[move,'recoil'])
+                if ((mon.hp - recoil) <= 0):
                     score = 0
                 else:
                     score -= recoil
             
             #   This is kind of petty, but we also have to be wary of crash damage, which
             #   in Gen 1 is always 1 HP, lol.
-            if (moveDex[move]['crash']):
+            if pd.notna(moveDex.at[move,'crash']):
                 score -= 1
             
             # If this move amounts to be optimal, we update the optimal move.
             if score > maxMoveScore:
-                optimalMove = move.index
+                optimalMove = move
+                maxMoveScore = score
     
-    # Finally, we return the optimal move.
-    return optimalMove
+    # Finally, we return the optimal move, or return struggle if nothing is picked.
+    if optimalMove == "":
+        return 'struggle'
+    else:
+        return optimalMove
+
+def monMovesFirst(mon, monMove, opp, oppMove):
+    if moveDex.at[monMove,'subcat'] == 'quick' and moveDex.at[oppMove,'subcat'] != 'quick':
+        return True
+    elif moveDex.at[oppMove,'subcat'] == 'quick' and moveDex.at[monMove,'subcat'] != 'quick':
+        return False
+    elif mon.speed > opp.speed:
+        return True
+    elif opp.speed > mon.speed:
+        return False
+    else:
+        return np.random.randint(low=0,high=1,size=1)
+
+
+def battle(mon, opp, level=50, log=False) -> int:
+    turnCounter = 0
+
+    if(log):
+        print("====  %s vs. %s at L%s  ====" % (mon.name, opp.name, level))
+
+    while(mon.hp > 0 and opp.hp > 0):
+        turnCounter += 1
+
+        if(log):
+            print("---TURN %s: %s: %s/%s, opponent %s: %s/%s" % (turnCounter, mon.name, mon.hp, pokeDex.at[mon.name, 'hp'], opp.name, opp.hp, pokeDex.at[opp.name,'hp']))
+
+        monMove = pickMove(mon, opp)
+        oppMove = pickMove(opp, mon)
+
+        if monMovesFirst(mon, monMove, opp, oppMove):
+            
+            if(np.random.binomial(1, float(moveDex.at[monMove,'accuracy'])/100)):
+                dmg = calculateDamage(mon, monMove, opp, level)
+                opp.hp -= dmg
+                if(log):
+                    print("%s uses %s. opponent %s takes %s damage!" % (mon.name, monMove, opp.name, dmg))
+                if opp.hp <= 0:
+                    if(log):
+                        print("opponent %s faints!" % opp.name)
+                    break
+            else:
+                if(log):
+                    print("%s uses %s, but it missed!" % (mon.name, monMove))
+
+
+            if(np.random.binomial(1, float(moveDex.at[oppMove,'accuracy'])/100)):
+                dmg = calculateDamage(opp, oppMove, mon, level)
+                mon.hp -= dmg
+                if(log):
+                    print("opponent %s uses %s. %s takes %s damage!" % (opp.name, oppMove, mon.name, dmg))
+                if mon.hp <= 0:
+                    if(log):
+                        print("%s faints!" % mon.name)
+                    turnCounter = (0 - turnCounter)
+                    break
+            else:
+                if(log):
+                    print("opponent %s uses %s, but it missed!" % (opp.name, oppMove))
+        
+        else:
+
+            if(np.random.binomial(1, float(moveDex.at[oppMove,'accuracy'])/100)):
+                dmg = calculateDamage(opp, oppMove, mon, level)
+                mon.hp -= dmg
+                if(log):
+                    print("opponent %s uses %s. %s takes %s damage!" % (opp.name, oppMove, mon.name, dmg))
+                if mon.hp <= 0:
+                    if(log):
+                        print("%s faints!" % mon.name)
+                    turnCounter = (0 - turnCounter)
+                    break
+            else:
+                if(log):
+                    print("opponent %s uses %s, but it missed!" % (opp.name, oppMove))
+
+
+            if(np.random.binomial(1, float(moveDex.at[monMove,'accuracy'])/100)):
+                dmg = calculateDamage(mon, monMove, opp, level)
+                opp.hp -= dmg
+                if(log):
+                    print("%s uses %s. opponent %s takes %s damage!" % (mon.name, monMove, opp.name, dmg))
+                if opp.hp <= 0:
+                    if(log):
+                        print("opponent %s faints!" % opp.name)
+                    break
+            else:
+                if(log):
+                    print("%s uses %s, but it missed!" % (mon.name, monMove))
+
+    return turnCounter
--- mechanics/test.py
+++ mechanics/test.py
@@ -1,7 +1,10 @@
 from asyncio.windows_events import NULL
+
+from numpy import record
 import gen1
 import pandas as pd
 import classes
+import string
 
 mon = 'bulbasaur'
 
@@ -43,10 +46,53 @@ bulbasaur = classes.Mon('bulbasaur', gen1.pokeDex)
 squirtle = classes.Mon('squirtle', gen1.pokeDex)
 charmander = classes.Mon('charmander', gen1.pokeDex)
 
-for col in gen1.learnDex.columns:
-    if col != 'index' and pd.notna(gen1.learnDex.at['bulbasaur', col]) \
-        and gen1.moveDex.at[col,'category'] == 'attack' \
-        and gen1.moveDex.at[col,'subcat'] not in ['suicide', 'counter', 'seed', 'rage', 'ko', 'random', 'status_dep'] \
-        and pd.notna(gen1.moveDex.at[col,'power']):
-        print("%s uses %s vs %s for %s/%s HP\n" % ('bulbasaur', col, 'charmander', gen1.calculateDamage(bulbasaur, col, charmander), gen1.pokeDex.at['charmander','hp']))
-        print("%s uses %s vs %s for %s/%s HP\n" % ('bulbasaur', col, 'squirtle', gen1.calculateDamage(bulbasaur, col, squirtle), gen1.pokeDex.at['charmander','hp']))
+# for col in gen1.learnDex.columns:
+#     if col != 'index' and pd.notna(gen1.learnDex.at['bulbasaur', col]) \
+#         and gen1.moveDex.at[col,'category'] == 'attack' \
+#         and gen1.moveDex.at[col,'subcat'] not in ['suicide', 'counter', 'seed', 'rage', 'ko', 'random', 'status_dep'] \
+#         and pd.notna(gen1.moveDex.at[col,'power']):
+#         print("%s uses %s vs %s for %s/%s HP\n" % ('bulbasaur', col, 'charmander', gen1.calculateDamage(bulbasaur, col, charmander), gen1.pokeDex.at['charmander','hp']))
+#         print("%s uses %s vs %s for %s/%s HP\n" % ('bulbasaur', col, 'squirtle', gen1.calculateDamage(bulbasaur, col, squirtle), gen1.pokeDex.at['charmander','hp']))
+
+
+print("==============TESTING MOVE PICKER=================") 
+# with open("log.txt","w") as f:
+#     for pokemon in list(gen1.pokeDex.index):
+#         opponent = classes.Mon(pokemon, gen1.pokeDex)
+
+#         print('~~~~VERSUS %s~~~~\n' % pokemon, file=f)
+#         print("Bulbasaur's optimal move is %s\n" % string.capwords(gen1.pickMove(bulbasaur, opponent)), file=f)
+#         print("Charmander's optimal move is %s\n" % string.capwords(gen1.pickMove(charmander, opponent)), file=f)
+#         print("Squirtle's optimal move is %s\n" % string.capwords(gen1.pickMove(squirtle, opponent)), file=f)
+
+
+print("==============BATTLE SIMULATOR=================")
+
+df = pd.DataFrame(columns=list(gen1.pokeDex.index))
+
+for mon1 in list(gen1.pokeDex.index):
+    recordLog = []
+
+    totWins = 0
+
+    for mon2 in list(gen1.pokeDex.index):
+        wins = 0
+        fights = 100
+
+        for i in range(fights):
+            pokemon = classes.Mon(mon1, gen1.pokeDex)
+            opponent = classes.Mon(mon2, gen1.pokeDex)
+
+            fightResults = gen1.battle(pokemon, opponent, log=False)
+
+            if fightResults > 0:
+                wins += 1
+                totWins += 1
+
+        recordLog.append(wins/float(fights))
+        print('%s wins %s/%s times vs. %s' % (mon1, wins, fights, mon2))
+
+    print('%s total win/loss ratio: %s' % (mon1, (float(totWins)/15100)))
+    df[mon1] = recordLog
+
+df.to_csv('results.csv')
