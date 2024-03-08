7e859136813fcfd570b6321abc1f32799261d383
hb-robo <brownhayden10@gmail.com>
2024-03-08 17:44:44 -0500

    chore(gen1): initial constants file

---
+++ data/gen1/gen1_constants.py
@@ -0,0 +1,32 @@
+# Pokemon Type Labels and Categories
+TYPES = [
+    'Normal', 'Fighting', 'Poison', 'Ground', 'Flying', 'Bug', 'Rock', 'Ghost',
+    'Fire', 'Water', 'Grass', 'Electric', 'Ice', 'Psychic', 'Dragon'
+]
+PHYS_TYPES = [
+    'Normal', 'Fighting', 'Poison', 'Ground', 'Flying', 'Bug', 'Rock', 'Ghost'
+]
+SPEC_TYPES = [
+    'Fire', 'Water', 'Grass', 'Electric', 'Ice', 'Psychic', 'Dragon'
+]
+TYPE_COLORS = {  # sourced from Pocket Monsters' Stadium (1998)
+    'Normal': '#a3a3a3',
+    'Fire': '#c13e2c',
+    'Fighting': '#c6703e',
+    'Water': '#4d9bda',
+    'Flying': '#4864b6',
+    'Grass': '#55ab38',
+    'Poison': '#7f54a4',
+    'Electric': '#c1a549',
+    'Ground': '#957b42',
+    'Psychic': '#c2589a',
+    'Rock': '#5c7979',
+    'Ice': '#71acc4',
+    'Bug': '#679a33',
+    'Dragon': '#4da396',
+    'Ghost': '#9e73b5'
+}
+
+# Statistic Labels and Names
+STATS = ['HP', 'Attack', 'Defense', 'Special', 'Speed', 'Accuracy', 'Evasion']
+BASE_STATS = ['HP', 'Attack', 'Defense', 'Special', 'Speed']
